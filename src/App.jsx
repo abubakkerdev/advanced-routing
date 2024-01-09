cc4341876bd955d3a67463dfc5a8f67c15e97691
abubakkerdev <abubakkerdev@gmail.com>
2024-01-09 16:37:55 +0600

    Latest Code

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,14 @@ import AccountLayout from "./components/account/AccountLayout";
 import AccountHome from "./pages/account/AccountHome";
 import AccountContact from "./pages/account/AccountContact";
 import AccountError from "./pages/errors/AccountError";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import SchoolUser, { user } from "./pages/school/SchoolUser";
+import SchoolUserError from "./pages/school/SchoolUserError";
+import AccountFrom from "./pages/account/AccountFrom";
+import userFromValidation from "./pages/account/accountFromvalidation";
+import AccountFromError from "./pages/account/AccountFromError";
+import AccountSuccess from "./pages/account/AccountSuccess";
 
 function App() {
   const router = createBrowserRouter(
@@ -28,19 +36,36 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-        <Route path="/*" element={<MainError />} />
 
         <Route path="/school" element={<SchoolLayout />}>
           <Route index element={<SchoolHome />} />
           <Route path="about" element={<SchoolAbout />} />
+          <Route
+            path="user"
+            element={<SchoolUser />}
+            loader={user}
+            errorElement={<SchoolUserError />}
+          />
         </Route>
         <Route path="/school/*" element={<SchoolError />} />
 
         <Route path="/account" element={<AccountLayout />}>
           <Route index element={<AccountHome />} />
           <Route path="contact" element={<AccountContact />} />
+          <Route path="success" element={<AccountSuccess />} />
+          <Route
+            path="from"
+            element={<AccountFrom />}
+            action={userFromValidation}
+            errorElement={<AccountFromError />}
+          />
         </Route>
         <Route path="/account/*" element={<AccountError />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/registrar" element={<Registration />} />
+
+        <Route path="/*" element={<MainError />} />
       </>
     )
   );
